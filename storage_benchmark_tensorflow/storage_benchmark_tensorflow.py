50dc16af8129b2083ee56de5842d45e89968068d
Claudio Fahey <claudio.fahey@dell.com>
2019-10-17 21:01:05 +0000

    Added driver to storage_benchmark_tensorflow.py

--- storage_benchmark_tensorflow/storage_benchmark_tensorflow.py
+++ storage_benchmark_tensorflow/storage_benchmark_tensorflow.py
@@ -24,6 +24,8 @@ import datetime
 import glob
 import json
 import os
+import subprocess
+import sys
 from tensorflow.contrib.data.python.ops import batching
 from tensorflow.contrib.data.python.ops import interleave_ops
 from tensorflow.contrib.data.python.ops import threadpool
@@ -40,7 +42,7 @@ def process_record(example_serialized):
     return num_bytes
 
 
-def create_iterator(input_file_spec, input_filenames, opts):
+def create_iterator(input_file_spec, input_filenames, args):
     if input_filenames:
         ds = tf.data.Dataset.from_tensor_slices(tf.convert_to_tensor(input_filenames))
     elif input_file_spec:
@@ -48,29 +50,29 @@ def create_iterator(input_file_spec, input_filenames, opts):
     else:
         raise ValueError('You must specify input_file_spec or input_filenames')
 
-    if opts.parallel_interleave_cycle_length:
+    if args.parallel_interleave_cycle_length:
         ds = ds.apply(
             interleave_ops.parallel_interleave(
-                lambda f: tf.data.TFRecordDataset(f, buffer_size=opts.dataset_buffer_size),
-                cycle_length=opts.parallel_interleave_cycle_length))
+                lambda f: tf.data.TFRecordDataset(f, buffer_size=args.dataset_buffer_size),
+                cycle_length=args.parallel_interleave_cycle_length))
     else:
         ds = ds.apply(tf.data.TFRecordDataset)
 
-    ds = ds.prefetch(buffer_size=opts.prefetch_records)
+    ds = ds.prefetch(buffer_size=args.prefetch_records)
     ds = ds.repeat()
     num_splits = 1
     ds = ds.apply(
         batching.map_and_batch(
             map_func=process_record,
-            batch_size=opts.batch_size,
+            batch_size=args.batch_size,
             num_parallel_batches=num_splits))
     ds = ds.prefetch(buffer_size=num_splits)
 
-    if opts.num_threads:
+    if args.num_threads:
         ds = threadpool.override_threadpool(
             ds,
             threadpool.PrivateThreadPool(
-                opts.num_threads, display_name='input_pipeline_thread_pool'))
+                args.num_threads, display_name='input_pipeline_thread_pool'))
         ds_iterator = ds.make_initializable_iterator()
         tf.add_to_collection(tf.GraphKeys.TABLE_INITIALIZERS,
                              ds_iterator.initializer)
@@ -79,12 +81,12 @@ def create_iterator(input_file_spec, input_filenames, opts):
     return ds_iterator
 
 
-def worker(rank, size, opts):
+def worker(rank, size, args):
     if rank == 0:
         print('storage_benchmark_tensorflow: BEGIN')
         print(datetime.datetime.utcnow())
 
-    metrics_file_name = os.path.join(opts.metrics_directory, 'storage_benchmark_tensorflow_metrics-%d.log' % rank)
+    metrics_file_name = os.path.join(args.metrics_directory, 'storage_benchmark_tensorflow_metrics-%d.log' % rank)
     with open(metrics_file_name, 'a') as metrics_file:
 
         hostname = socket.gethostname()
@@ -93,10 +95,10 @@ def worker(rank, size, opts):
         tf.set_random_seed(rank + 1)
 
         # Round robin the input file spec. This allows multiple mount points to be used.
-        input_file_spec = opts.input_file_specs[hvd.local_rank() % len(opts.input_file_specs)]
+        input_file_spec = args.input_file_specs[hvd.local_rank() % len(args.input_file_specs)]
         print('rank=%3d: %s: input_file_spec=%s' % (rank, hostname, input_file_spec))
 
-        if opts.round_robin_files:
+        if args.round_robin_files:
             # Distribute sets of file names evenly over all processes and without overlap.
             all_input_filenames = sorted(glob.glob(input_file_spec))
             num_files = len(all_input_filenames)
@@ -117,7 +119,7 @@ def worker(rank, size, opts):
         # Build execution graph.
         #
 
-        ds_iterator = create_iterator(input_file_spec, input_filenames, opts)
+        ds_iterator = create_iterator(input_file_spec, input_filenames, args)
 
         # num_bytes_tensor is an int64 tensor of shape (batch_size).
         num_bytes_tensor = ds_iterator.get_next()
@@ -126,7 +128,7 @@ def worker(rank, size, opts):
         num_bytes_for_step_tensor = tf.reduce_sum(num_bytes_tensor)
 
         # The following operations are used to synchronize the processes when running in sync mode.
-        if opts.sync:
+        if args.sync:
             stop_flag_placeholder = tf.placeholder(tf.bool, shape=())
             stop_flag_broadcast_tensor = hvd.broadcast(stop_flag_placeholder, 0, 'stop_flag_broadcast')
             num_bytes_for_step_placeholder = tf.placeholder(tf.int64, shape=())
@@ -165,19 +167,19 @@ def worker(rank, size, opts):
             t0_tensor = hvd.broadcast(t0_tensor, 0, 't0')
             t0 = session.run(t0_tensor)
 
-            start_time = t0 + opts.warmup_sec
-            stop_time = start_time + opts.run_sec
+            start_time = t0 + args.warmup_sec
+            stop_time = start_time + args.run_sec
             step = 0
             warmed_up = False
             num_records = 0
             num_bytes = 0
             total_bytes = 0
-            next_report_time = time.time() + opts.report_period_sec
+            next_report_time = time.time() + args.report_period_sec
 
-            if opts.throttle_total_rate_bytes_per_sec:
-                throttle_rate_bytes_per_sec = opts.throttle_total_rate_bytes_per_sec / size
+            if args.throttle_total_rate_bytes_per_sec:
+                throttle_rate_bytes_per_sec = args.throttle_total_rate_bytes_per_sec / size
                 burst_sec = 1.0
-                throttle = TokenBucket(tokens=opts.throttle_rate_bytes_per_sec*burst_sec, fill_rate=throttle_rate_bytes_per_sec)
+                throttle = TokenBucket(tokens=args.throttle_rate_bytes_per_sec*burst_sec, fill_rate=throttle_rate_bytes_per_sec)
             else:
                 throttle = None
 
@@ -204,7 +206,7 @@ def worker(rank, size, opts):
 
                 step_dt = time.time() - t
 
-                if (warmed_up or step >= 1) and step_dt > opts.warn_latency_sec:
+                if (warmed_up or step >= 1) and step_dt > args.warn_latency_sec:
                     print('rank=%3d: %s: WARNING: step %d took %0.3f seconds' %
                           (rank, hostname, step, step_dt))
                     next_report_time = 0.0
@@ -217,7 +219,7 @@ def worker(rank, size, opts):
                 # it shares gradients.
                 # Also coordinate the stop flag so all processes stop at the same step.
                 sync_dt = 0.0
-                if opts.sync:
+                if args.sync:
                     t = time.time()
                     total_bytes_for_step, stop_flag = session.run(
                         [total_bytes_for_step_tensor, stop_flag_broadcast_tensor],
@@ -235,13 +237,13 @@ def worker(rank, size, opts):
                               (rank, hostname, step, sync_dt))
                         next_report_time = 0.0
 
-                num_records += opts.batch_size
+                num_records += args.batch_size
                 num_bytes += num_bytes_for_step
                 t = time.time()
 
                 metrics = {
                     '@timestamp': datetime.datetime.utcnow().isoformat() + 'Z',
-                    'batch_size': opts.batch_size,
+                    'batch_size': args.batch_size,
                     'rank': rank,
                     'hostname': hostname,
                     'step': step,
@@ -255,13 +257,13 @@ def worker(rank, size, opts):
 
                 if t >= next_report_time:
                     dt = t - t0
-                    if not opts.sync:
+                    if not args.sync:
                         records_per_sec = num_records / dt
                         bytes_per_sec = num_bytes / dt
                         MB_per_sec = bytes_per_sec / 1e6
                         print('rank=%3d: warmed_up=%d, step=%6d, records/sec=%8.0f, MB/sec=%11.3f, records=%10d, bytes=%15d, dt=%9.3f' %
                               (rank, warmed_up, step, records_per_sec, MB_per_sec, num_records, num_bytes, dt))
-                    if opts.sync:
+                    if args.sync:
                         if rank == 0:
                             total_records = num_records * size
                             records_per_sec = total_records / dt
@@ -269,13 +271,13 @@ def worker(rank, size, opts):
                             MB_per_sec = bytes_per_sec / 1e6
                             print('TOTAL:    warmed up=%d, step=%6d, records/sec=%8.0f, MB/sec=%11.3f, records=%10d, bytes=%15d, dt=%9.3f' %
                                 (warmed_up, step, records_per_sec, MB_per_sec, total_records, total_bytes, dt))
-                    next_report_time = t + opts.report_period_sec
+                    next_report_time = t + args.report_period_sec
 
                 # Throttle byte rate.
                 if throttle:
                     while not throttle.consume(num_bytes_for_step):
                         # print('sleeping')
-                        time.sleep(opts.throttle_sleep_sec)
+                        time.sleep(args.throttle_sleep_sec)
 
                 if stop_flag:
                     print('rank=%3d: %s: complete at step %d' % (rank, hostname, step))
@@ -291,24 +293,98 @@ def worker(rank, size, opts):
             total_steps, total_bytes = session.run([total_steps_tensor, total_bytes_tensor])
             if rank == 0:
                 dt = stop_time - start_time
-                num_records = total_steps * opts.batch_size
+                num_records = total_steps * args.batch_size
                 records_per_sec = num_records / dt
                 total_GB = total_bytes / 1e9
                 bytes_per_sec = total_bytes / dt
                 MB_per_sec = bytes_per_sec / 1e6
                 print('FINAL: number of processes: %12d' % size)
-                print('FINAL: batch size:          %12d' % opts.batch_size)
-                print('FINAL: sync:                %12s' % opts.sync)
-                print('FINAL: round robin files:   %12s' % opts.round_robin_files)
+                print('FINAL: batch size:          %12d' % args.batch_size)
+                print('FINAL: sync:                %12s' % args.sync)
+                print('FINAL: round robin files:   %12s' % args.round_robin_files)
                 print('FINAL: number of records:   %12d' % num_records)
                 print('FINAL: GB:                  %12.3f' % total_GB)
                 print('FINAL: elapsed sec:         %12.3f' % dt)
                 print('FINAL: records/sec:         %12.0f' % records_per_sec)
                 print('FINAL: MB/sec:              %12.3f' % MB_per_sec)
-                print('FINAL: options:             %s' % str(opts))
+                print('FINAL: options:             %s' % str(args))
 
         if rank == 0:
             print('storage_benchmark_tensorflow: END')
+
+
+def driver(args):
+    print('storage_benchmark_tensorflow: BEGIN')
+    print(datetime.datetime.utcnow())
+
+    mpi_hosts = ','.join(['%s:%d' % (h, args.npernode) for h in args.host])
+    num_hosts = len(args.host)
+
+    if args.install:
+        cmd = [
+            'mpirun',
+            '--n', str(num_hosts),
+            '-npernode', '1',
+            '-allow-run-as-root',
+            '--host', mpi_hosts,
+            '-mca', 'plm_rsh_agent', 'ssh',
+            '-mca', 'plm_rsh_args', '"-p 2222"',
+            'pip', 'install', '--user', '--requirement', 'requirements.txt',
+        ]
+        print(' '.join(cmd))
+        subprocess.run(cmd, check=True)
+
+    # Flush Isilon cache.
+    if args.flush:
+        cmd = [
+            'ssh',
+            '-p', '22',
+            '%s@%s' % (args.isilon_user, args.isilon_host),
+            'isi_for_array', 'isi_flush',
+        ]
+        print(' '.join(cmd))
+        subprocess.run(cmd, check=True)
+
+    # Drop caches on all slave nodes.
+    if args.flush:
+        cmd = [
+            'mpirun',
+            '--n', str(num_hosts),
+            '-npernode', '1',
+            '-allow-run-as-root',
+            '--host', mpi_hosts,
+            '-mca', 'plm_rsh_agent', 'ssh',
+            '-mca', 'plm_rsh_args', '"-p 2222"',
+            '/scripts/drop_caches.sh',
+        ]
+        print(' '.join(cmd))
+        subprocess.run(cmd, check=True)
+
+    # Run the same command that launched this driver but with mpirun.
+    cmd = [
+        'mpirun',
+        '--allow-run-as-root',
+        '--np', str(args.np),
+        '--host', mpi_hosts,
+        '-map-by', 'node',
+        '-bind-to', 'socket',
+        '--report-bindings',
+        '-mca', 'plm_rsh_agent', 'ssh',
+        '-mca', 'plm_rsh_args', '"-p 2222"',
+        '-mca', 'pml', 'ob1',
+        '-mca', 'btl', '^openib',
+        '-mca', 'btl_tcp_if_exclude', 'lo,docker0',
+        '-x', 'LD_LIBRARY_PATH',
+        '-x', 'PATH',
+        '-x', 'CUDA_VISIBLE_DEVICES=""',
+    ]
+    cmd += sys.argv
+    cmd += ['--worker']
+    print(' '.join(cmd))
+    subprocess.run(cmd, check=True)
+
+    print(datetime.datetime.utcnow())
+    print('storage_benchmark_tensorflow: END')
 
 
 def main():
@@ -318,25 +394,40 @@ def main():
     )
     parser.add('--batch_size', type=int, default=256, help='Number of records per batch')
     parser.add('--config', '-c', required=False, is_config_file=True, help='config file path')
-    parser.add('--dataset_buffer_size', type=int, nargs='?', help='')
+    parser.add('--dataset_buffer_size', type=int, nargs='?')
+    parser.add('--flush', action='store_true', help='Flush caches')
+    parser.add('--host', '-H', action='append', required=True, help='List of hosts on which to invoke processes.')
     parser.add('--input_file_specs', '-i', action='append', help='Input file spec', required=True)
-    parser.add('--metrics_directory', default='/tmp', help='')
+    parser.add('--install', action='store_true')
+    parser.add('--isilon_host', action='store',
+               help='IP address or hostname of an Isilon node. You must enable password-less SSH.')
+    parser.add('--isilon_user', action='store', default='root',
+               help='SSH user used to connect to Isilon.')
+    parser.add('--metrics_directory', default='/tmp')
+    parser.add('--noop', action='store_true')
+    parser.add('--np', type=int, default=1)
+    parser.add('--npernode', action='store', type=int, default=80, help='On each node, launch this many processes.')
     parser.add('--num_threads', type=int, default=0, help='Number of threads')
-    parser.add('--parallel_interleave_cycle_length', type=int, default=0, help='')
-    parser.add('--prefetch_records', type=int, nargs='?', help='')
+    parser.add('--parallel_interleave_cycle_length', type=int, default=0)
+    parser.add('--prefetch_records', type=int, nargs='?')
     parser.add('--report_period_sec', type=float, default=2.0, help='Report statistics with this period in seconds')
-    parser.add('--round_robin_files', action='store_true', help='')
+    parser.add('--round_robin_files', action='store_true')
     parser.add('--run_sec', type=float, default=60*60*4, help='Run time in seconds')
     parser.add('--sync', action='store_true', help='Synchronize workers after each batch')
-    parser.add('--throttle_sleep_sec', type=float, default=0.01, help='')
+    parser.add('--throttle_sleep_sec', type=float, default=0.01)
     parser.add('--throttle_total_rate_bytes_per_sec', type=float, default=0, help='If 0, unthrottled')
     parser.add('--warmup_sec', type=float, default=10.0, help='Warm-up time in seconds')
     parser.add('--warn_latency_sec', type=float, default=4.0, help='Warn if read latency exceeds this many seconds')
-    opts = parser.parse_args()
-    print('storage_benchmark_tensorflow: Options: %s' % str(opts))
+    parser.add('--worker', action='store_true')
+    args = parser.parse_args()
+    print('storage_benchmark_tensorflow: Options: %s' % str(args))
 
-    hvd.init()
-    worker(hvd.rank(), hvd.size(), opts)
+    if args.worker:
+        if not args.noop:
+            hvd.init()
+            worker(hvd.rank(), hvd.size(), args)
+    else:
+        driver(args)
 
 
 if __name__ == '__main__':
