c4bb3f3316a8930a72a97143111f33ecd3185ed1
Claudio Fahey <claudio.fahey@dell.com>
2019-10-20 04:20:13 +0000

    run_benchmark.sh now creates log files with a timestamp

--- run_benchmark.sh
+++ run_benchmark.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 set -ex
+LOGFILE="/imagenet-scratch/logs/run_benchmark_$(date "+%Y%m%dT%H%M%S").log"
 python -u ./run_benchmark.py $* \
-|& tee -a /imagenet-scratch/logs/run_benchmark.log
+|& tee -a "${LOGFILE}"
+echo run_benchmark.sh: END: Log file is ${LOGFILE}
