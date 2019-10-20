2982cda6f2242fecf45166af17e4f72c3d321883
Claudio Fahey <claudio.fahey@dell.com>
2019-10-20 04:48:31 +0000

    run_benchmark_all.sh now accepts a list of yaml files

--- run_benchmark_all.sh
+++ run_benchmark_all.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 LOGFILE="/imagenet-scratch/logs/run_benchmark_all_$(date "+%Y%m%dT%H%M%S").log"
-echo -n run_benchmark_*.yaml run_benchmark_*.yaml run_benchmark_*.yaml | \
-xargs -d " " -i -P 1 python -u run_benchmark.py -c {} \
-|& tee -a "${LOGFILE}"
+for config in "$@"
+do
+    python -u run_benchmark.py -c "${config}" |& tee -a "${LOGFILE}"
+done
 echo run_benchmark_all.sh: END: Log file is ${LOGFILE}
