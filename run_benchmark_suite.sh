7b03d31d54dfbf76d1c511c9b17c00ba239b1d60
Claudio Fahey <claudio.fahey@dell.com>
2019-10-24 02:47:42 +0000

    Removed obsolete files - benchmark_suite_*, etc.

--- run_benchmark_suite.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-SCRIPT_DIR=$(dirname $0)
-LOGFILE="/imagenet-scratch/logs/run_benchmark_all_$(date "+%Y%m%dT%H%M%S").log"
-for config in "$@"
-do
-    python -u "${SCRIPT_DIR}/run_benchmark.py" -c "${config}" |& tee -a "${LOGFILE}"
-done
-echo run_benchmark_all.sh: END: Log file is ${LOGFILE}
