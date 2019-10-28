3e3ba0327c8eba124524d98e053d60ea155d85a5
Claudio Fahey <claudio.fahey@dell.com>
2019-10-28 01:23:22 +0000

    Removed obsolete Parabricks scripts

--- parabricks/slurm_test1.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-echo slurm_test1.sh: $0: $(hostname): CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}: BEGIN
-pwd
-ls -lh
-sleep 10s
-echo slurm_test1.sh: $(hostname): CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}: END
