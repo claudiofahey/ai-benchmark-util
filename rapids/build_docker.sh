5940c294123f745ca92f0cf3400622c7911a1ee7
Claudio Fahey <claudio.fahey@dell.com>
2019-11-23 04:41:57 +0000

    Now using nightly RAPIDS container

--- rapids/build_docker.sh
+++ rapids/build_docker.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 set -ex
-IMAGE_TAG=${IMAGE_TAG:-claudiofahey/rapidsai:0.10-cuda10.0-runtime-ubuntu18.04-custom}
-docker build -t ${IMAGE_TAG} .
-docker push ${IMAGE_TAG}
+# Below digest is for cuda10.0-runtime-ubuntu18.04 dated 2019-11-22
+FROM_SHA256=a359097c3c18a534b91557d5abe772c73ef57d11de3dfb632e1516b0a01745f1
+FROM_IMAGE=rapidsai/rapidsai-nightly@sha256:${FROM_SHA256}
+TO_IMAGE=claudiofahey/rapidsai:${FROM_SHA256}
+docker build -t ${TO_IMAGE} --build-arg FROM_IMAGE=${FROM_IMAGE} .
+docker push ${TO_IMAGE}
